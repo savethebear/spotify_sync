df88ba93de5f890122bec8c32f589d523b474c8b
Christopher Lee <890136@swansea.ac.uk>
2020-06-01 19:52:38 +0100

    Stored token after authorisation

--- browser_extension/background.js
+++ browser_extension/background.js
@@ -5,6 +5,8 @@ chrome.runtime.onInstalled.addListener(function () {
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.access_token) {
-            console.log(request.token);
+            console.log(request.access_token);
+            chrome.storage.sync.set({ spotify_session_token: request.access_token });
         }
-    });
+    }
+);
