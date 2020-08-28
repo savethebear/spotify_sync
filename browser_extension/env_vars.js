93ecf2fa399eed4666266bf6e40546b5cad87ab8
savethebear <christopherlee229@gmail.com>
2020-08-28 17:41:29 +0800

    added token keys.

--- browser_extension/env_vars.js
+++ browser_extension/env_vars.js
@@ -1,5 +1,10 @@
 class ConstantVariables {
     constructor() {
         this.server_ip = "192.168.1.30:3000";
+        
+        // saved access token keys
+        this.LOCALSTORAGE_ACCESS_TOKEN_KEY = 'spotify-sync-access-token';
+        this.LOCALSTORAGE_ACCESS_TOKEN_EXPIRY_KEY = "spotify-sync-access-token-expires-in";
+        this.LOCALSTORAGE_REFRESH_TOKEN_KEY = "spotify-sync-refresh-token";
     }
 }
